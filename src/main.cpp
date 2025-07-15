49ca41aaef3a4272c5bdf466a9deca5d2eaab92b
myooker <lovasq@yandex.ru>
2025-07-16 01:10:33 +0300

    Make video playable

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,29 @@ int main() {
         std::cerr << "Unable to open video file" << std::endl;
     }
 
-    video.read(frame);
+    // cv::namedWindow("Image", cv::WINDOW_NORMAL);
+    // cv::resizeWindow("Image", 1080, 1920);
+    // cv::imshow("Image",frame);
 
-    cv::namedWindow("Image", cv::WINDOW_NORMAL);
-    cv::resizeWindow("Image", 1080, 1920);
-    cv::imshow("Image",frame);
-
-    // Exit program only when 'q' is pressed
     while (true) {
-        int key = cv::waitKey(0);
-        if (key == 'q') break;
+        video.read(frame);
+
+        if (frame.empty()) {
+            std::cerr << "Error! Blank frame grabbed\n";
+            break;
+        }
+        cv::namedWindow("Video", cv::WINDOW_NORMAL);
+        cv::resizeWindow("Video", 504, 896);
+        cv::imshow("Video", frame);
+        if (cv::waitKey(15) >= 0)
+            break;
     }
+
+    // // Exit program only when 'q' is pressed
+    // while (true) {
+    //     int key = cv::waitKey(0);
+    //     if (key == 'q') break;
+    // }
 
     video.release();
 
