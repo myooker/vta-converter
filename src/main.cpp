65da2d859783cdb6dd27606480907d1fbb8bf657
myooker <lovasq@yandex.ru>
2025-07-17 00:55:38 +0300

    refactor: clean up code and improve readability

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,69 @@
 #include <opencv4/opencv2/imgproc.hpp>
 #include <opencv4/opencv2/videoio.hpp>
 
+std::string getPath();
 
 namespace vta {
     constexpr std::string_view palette{" .:-=+*#%@"}; // From lightest to darker
-    const cv::Mat frame{ cv::imread("/home/myooker/Pictures/badapple.jpg", cv::IMREAD_REDUCED_GRAYSCALE_8) };
+    // Hard-coded path to the picture.
+    cv::Mat frame{
+        cv::imread(
+            "/home/myooker/Pictures/smart dude with glasses wallpaper.png",
+            cv::IMREAD_REDUCED_GRAYSCALE_8)
+    };
+    // cv::Mat frame{ cv::imread(getPath(), cv::IMREAD_REDUCED_GRAYSCALE_4) };
 }
 
 enum Constants {
     MAX_GRAY_VALUE = 255,
 };
 
+std::string getPath() {
+    std::string path{};
+    std::cout << "Please enter full path to an image\n> ";
+    std::getline(std::cin, path);
+
+    return path;
+}
+
 std::size_t getPaletteIndex(const unsigned int pixelColor) {
     return (pixelColor * (vta::palette.size()-1) / MAX_GRAY_VALUE);
 }
 
-std::string convertRow(const cv::Mat& frame, int govno) {
+std::vector<std::string> frameConvertToAscii(const cv::Mat& frame) {
     if (frame.empty()) {
         std::cerr << "Error! No image detected!\nExiting the program..." << std::endl;
         std::exit(1);
     }
 
-    uchar* ptr = frame.data;
-    const std::size_t rowSize{ static_cast<std::size_t>(frame.rows - 1) };
-    std::string asciiRow{};
+    const std::size_t frameRowSize{ static_cast<std::size_t>( frame.rows )};    // Get frame's row f(x) size
+    const std::size_t frameColSize{ static_cast<std::size_t>( frame.cols )};    // Get frame's column f(y) size
+    std::vector<std::string> asciiFrame{};
+    asciiFrame.resize(frameRowSize);    // Resizing std::string's in vector to frame's row f(x) size, without it the program crashes
 
-    for (std::size_t i{}; i <= rowSize; i++) {
-        // constexpr int defaultRow{ 0 };
-        unsigned int pixelValue{};
-        pixelValue = frame.at<uchar>(govno, static_cast<int>(i));
-        asciiRow.push_back(vta::palette[getPaletteIndex(pixelValue)]);
+    // At first, we're starting with a row
+    for (std::size_t rowCount{}; rowCount < frameRowSize; rowCount++) {
+
+        // Then, we're filling our row with columns, f(y) and print it to the terminal
+        for (std::size_t colCount{}; colCount < frameColSize; colCount++) {
+            unsigned int pixelValue{};
+            pixelValue = frame.at<uchar>(static_cast<int>(rowCount), static_cast<int>(colCount));
+            asciiFrame[rowCount].push_back( vta::palette[getPaletteIndex(pixelValue)] );
+            std::cout << asciiFrame[rowCount][colCount];
+        }
+        // After filling the row, we go to the new line and filling the next row again
+        std::cout << '\n';
     }
 
-    return asciiRow;
+    return asciiFrame;
 }
 
 int main() {
+    cv::resize(vta::frame, vta::frame, cv::Size(), 1, 0.5);
     std::cout << "Number of rows: " << vta::frame.rows << '\n';
     std::cout << "Number of columns: " << vta::frame.cols << '\n';
 
-    for (int i{}; i < vta::frame.cols; i++) {
-        std::cout << convertRow(vta::frame, i) << '\n';
-    }
-
-    // int key{};
-    // while (true) {
-    //     key = cv::waitKey(0);
-    //     if (key == 'q') {
-    //         // video.release();
-    //         std::exit(0);
-    //     }
-    // }
+    frameConvertToAscii(vta::frame);
 
 
     return 0;
