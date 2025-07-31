f11d688a57a374722936f5e9a956f39bd4f5792a
myooker <lovasq@yandex.ru>
2025-07-31 22:35:25 +0300

    refactor: cli parsing and improve argument error handling

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,18 @@ namespace Program{
     enum FILE_ERROR {
         NEGRO_ERROR,
         NOT_EXIST,
+
         WRONG_FORM,
         WRONG_PATH,
+
+        NO_OPTION,
+        NO_PATH,
+    };
+
+    enum ARG_INDEX {
+        OPTION = 2,
+        PATH = 3,
+
     };
 }
 
@@ -40,7 +50,6 @@ struct ProgramSettings {
     std::string videoPath{"None"};
     bool isVerbose{ false };
     bool isHelp{ false };
-
 };
 
 bool isCorrectPath(const std::string_view& path, Program::FILE_ERROR &errorType) {
@@ -74,39 +83,62 @@ bool isCorrectPath(const std::string_view& path, Program::FILE_ERROR &errorType)
 }
 
 void invalidFile(const Program::FILE_ERROR ERROR_TYPE) {
+    using namespace Program;
     switch (ERROR_TYPE) {
-        case Program::WRONG_PATH:
-            std::cout << "error: invalid file path\n\n"
-            << "Make sure to:\n"
-            << "- Use absolute path to an existing file\n"
-            << "- Avoid using unsupported character or symbols\n\n"
-            << "Example:\n"
-            << "\t/home/username/video/sample.mp4\n"
-            << "\t~/video/sample.mp4" << std::endl;
-            return;
-        case Program::NOT_EXIST:
-            std::cout << "error: the file does not exist\n\n"
-            << "Make sure to:\n"
-            << "- Use absolute path to an existing file\n"
-            << "- Avoid using unsupported character or symbols\n\n"
-            << "Example:\n"
-            << "\t/home/username/video/sample.mp4\n"
-            << "\t~/video/sample.mp4" << std::endl;
-            return;
-        case Program::WRONG_FORM:
-            std::cout << "error: incorrect video format\n\n"
-            << "Make sure to:\n"
-            << "- Use absolute path to an existing file\n"
-            << "- Avoid using unsupported character or symbols\n\n"
-            << "Example:\n"
-            << "\t/home/username/video/sample.mp4\n"
-            << "\t~/video/sample.mp4" << std::endl;
-            return;
+        case WRONG_FORM:
+            std::cout << "error: incorrect video format\n\n";
+            break;
+        case WRONG_PATH:
+            std::cout << "error: invalid file path\n\n";
+            break;
+        case NOT_EXIST:
+            std::cout << "error: the file does not exist\n\n";
+            break;
+        case NO_PATH:
+            std::cout << "error: no path provided\n"
+                    << "hint: use --file <PATH> to specify a video file\n\n";
+            break;
+        case NO_OPTION:
+            std::cout << "error: missing required option\n"
+                     << "hint: expected --file <PATH> or --cache <PATH> after the command\n\n";
         default: ;
     }
+
+    std::cout << "Make sure to:\n"
+    << "- Use absolute path to an existing file\n"
+    << "- Avoid using unsupported character or symbols\n\n"
+    << "Example:\n"
+    << "\t/home/username/video/sample.mp4\n"
+    << "\t~/video/sample.mp4" << std::endl;
+
+    std::exit(1);
 }
 
+void playCommand(const char **argv, ProgramSettings &currentSettings) {
+    using namespace Program;
+    if (argv[OPTION] == nullptr) {
+        invalidFile(NO_OPTION);
+        return;
+    }
 
+    if (argv[PATH] == nullptr) {
+        invalidFile(NO_PATH);
+        return;
+    }
+
+    FILE_ERROR ERROR_TYPE{};
+
+    const std::string_view argument{ argv[2] };
+    const std::string_view path{ argv[3] };
+
+    if (argument == "--file" || argument == "-f") {
+        if (isCorrectPath(path, ERROR_TYPE)) {
+            currentSettings.videoPath = path;
+        } else {
+            invalidFile(ERROR_TYPE);
+        }
+    }
+}
 
 int main(const int argc, const char **argv) {
     std::atexit(ansi::showCursor);
@@ -115,31 +147,31 @@ int main(const int argc, const char **argv) {
     using namespace ftxui;
     using namespace std::chrono_literals;
     ProgramSettings currentSettings{};
-
-
-    for (int i{ 1 }; i < argc; i++) {
-        if (std::string_view{ argv[i] } == Program::commandPlay && argc > 3) {
-
-            if (const std::string_view fileArg{argv[i+1]}; fileArg == "-f" || fileArg == "--file") {
-                if (Program::FILE_ERROR ERROR_TYPE{}; isCorrectPath(std::string_view{argv[i+2]}, ERROR_TYPE)) {
-                    currentSettings.videoPath = argv[i+2];
-                } else {
-                    invalidFile(ERROR_TYPE);
-                    return 1;
-                }
-            } else {
-
-            }
-            if (const std::string_view cacheArg{argv[i+1]}; cacheArg == "-c" || cacheArg == "--cache") {
-
-            }
-        }
-
-        if (std::string_view {argv[i]} == Program::commandHelp) {
-            //printHelp();
-            return 0;
-        }
-    }
+    playCommand(argv, currentSettings);
+    
+    // for (int i{ 1 }; i < argc; i++) {
+    //     if (std::string_view{ argv[i] } == Program::commandPlay && argc > 3) {
+    //
+    //         if (const std::string_view fileArg{argv[i+1]}; fileArg == "-f" || fileArg == "--file") {
+    //             if (Program::FILE_ERROR ERROR_TYPE{}; isCorrectPath(std::string_view{argv[i+2]}, ERROR_TYPE)) {
+    //                 currentSettings.videoPath = argv[i+2];
+    //             } else {
+    //                 invalidFile(ERROR_TYPE);
+    //                 return 1;
+    //             }
+    //         } else {
+    //
+    //         }
+    //         if (const std::string_view cacheArg{argv[i+1]}; cacheArg == "-c" || cacheArg == "--cache") {
+    //
+    //         }
+    //     }
+    //
+    //     if (std::string_view {argv[i]} == Program::commandHelp) {
+    //         //printHelp();
+    //         return 0;
+    //     }
+    // }
 
     std::cout << "isVerbose: " << currentSettings.isVerbose << '\n'
                 << "isHelp: " << currentSettings.isHelp << '\n'
